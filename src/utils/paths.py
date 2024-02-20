94b66b298dc1856860d40e7987193cbc0dc80450
TGusciora <tgusciora@gmail.com>
2024-02-20 13:46:45 +0100

    2024/02/20 update
    - added paths dictionary function creation
    - updated requirements (to be verified later)
    - created initial notebook with downloading data from stooq.pl

---
+++ src/utils/paths.py
@@ -0,0 +1,32 @@
+import os
+
+def paths_dictionary():
+    """
+    Creating dictionary with paths in the parent/project directory.
+
+    Crawls through folders except names starting with '.' and '_'.
+    Returns a dictionary with folder paths separated with "_" as 
+    keys and their true paths as values.
+
+    Notes
+    -------------------
+    Required libraries: \n
+    * import os
+
+    Returns
+    --------
+    data : dictionary
+            Dictionary with paths.
+    """
+    parent_path = os.path.dirname(os.getcwd())
+    paths_dict = {}
+    for root, dirs, files in os.walk(parent_path):
+        # Delete all folders starting with '.' and '_' and their subfolders
+        dirs[:] = [d for d in dirs if not d.startswith('.') and not d.startswith('_')]
+
+        for dir in dirs:
+            folder_path = os.path.join(root, dir)
+            # Tworzymy klucz, usuwając ścieżkę rodzica i zamieniając separatory na "_"
+            dict_key = folder_path.replace(parent_path, "").strip(os.sep).replace(os.sep, "_")
+            paths_dict[dict_key] = folder_path
+    return paths_dict

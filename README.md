7310f87af992c56a6666508f95d4528931e29881
TGusciora <tgusciora@gmail.com>
2024-03-09 14:10:25 +0100

    2024/03/09 update
    - going through requirements and setup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # 002_post_get_organized
-How to structure your project? Explaining best practices for organizing data science projects in Python using Apple Inc. short-term price prediction as an example. Project goals: replicability, maintainability, portability, clarity.
+How to structure your project? Explaining best practices for organizing data science projects in Python using Apple Inc. short-term price prediction as an example. Project goals: replicability, maintainability, portability, clarity. Find out more here: https://tgusciora.github.io/ & https://demistyfai.substack.com/
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='1.0.0',
-    description='How to structure your project? Explaining best practices for organizing data science projects in Python using Apple Inc. short-term price prediction as an example. Project goals: replicability, maintainability, portability, clarity.',
+    description='How to structure your project? Explaining best practices for organizing data science projects in Python using Apple Inc. short-term price prediction as an example. Project goals: replicability, maintainability, portability, clarity. Find out more here: https://tgusciora.github.io/ & https://demistyfai.substack.com/',
     author='Tomasz Gusciora',
     license='MIT',
 )

73839730d48a0b900df12d139be01f805061bc54
TGusciora <tgusciora@gmail.com>
2024-03-28 08:53:20 +0100

    2024/03/28 update
    - make dataset function update
    - tidying up

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,23 +1,38 @@
 import pandas as pd
+import os
 import datetime as dt
 
-
-def stooq_import(ticker: str):
+def stooq_import(ticker: str, path: str):
     """
-    Fetches historical stock data from Stooq for a given ticker.
+    Fetches historical stock data from Stooq for a given ticker. If data is not
+    present locally, it downloads the data and saves it as a CSV file in the specified
+    folder. If the data is already present, it loads the data from the CSV file.
 
     Args:
         ticker (str): Ticker symbol of the stock.
+        paths (dict): Dictionary containing the path to save the raw data.
 
     Returns:
         pandas.DataFrame: DataFrame containing the historical stock data.
     """
-    url = f"https://stooq.com/q/d/l/?s={ticker}&i=d"
-    data = pd.read_csv(url)
-    data.columns = [col.lower() for col in data.columns]
-    data["date"] = pd.to_datetime(data["date"])
+    # Generate the file path
+    file_path = os.path.join(path, ticker + ".csv")
+    
+    # Check if the file exists
+    if os.path.isfile(file_path):
+        print(f"Loading data from {file_path}")
+        data = pd.read_csv(file_path)  # Load data from CSV file
+        
+    else:
+        print(f"Fetching and saving data for {ticker}")
+        url = f"https://stooq.com/q/d/l/?s={ticker}&i=d"
+        data = pd.read_csv(url)  # Fetch data
+        # if you're reading this carved out in stone in front of a cave, I am sorry.
+        # You have to be connected to the Internet to download data.
+        data.columns = [col.lower() for col in data.columns]
+        data.to_csv(file_path, index=False)  # Save data as CSV file
+
+    data["date"] = pd.to_datetime(data["date"])    
     return data
 
 
-# if you're reading this carved out in stone in front of a cave, I am sorry.
-# You have to be connected to the Internet to download data.
